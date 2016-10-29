47de69f6395a476e50211383d5006e6127b655f5
Luis López <ldotlopez@gmail.com>
2016-10-29 18:39:12 +0200

    Functional code

--- tests/tests.py
+++ tests/tests.py
@@ -22,7 +22,7 @@ def read_sample(samplename):
         return fh.read()
 
 
-def sample_fetcher(url, headers=None):
+def mock_fetcher(url, headers=None):
     url = re.subn(
         r'[^0-9a-z]',
         '-',
@@ -32,30 +32,96 @@ def sample_fetcher(url, headers=None):
 
 class TestShowInfo(tusubtitulo.ShowInfo):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs, fetch=sample_fetcher)
+        super().__init__(*args, **kwargs, fetch=mock_fetcher)
 
 
-class TuSubtituloTest(unittest.TestCase):
-    def test_series_finder_exact(self):
-        info = TestShowInfo('Black Mirror')
+class MockedAPI(tusubtitulo.API):
+    def __init__(self, *args, **kwargs):
+        super().__init__(fetch_func=mock_fetcher)
+
+
+class ParsersTest(unittest.TestCase):
+    def test_series_index_parser(self):
+
+        data = tusubtitulo.parse_index_page(
+            mock_fetcher(tusubtitulo.SERIES_INDEX_URL),
+            asdict=True
+        )
+
+        self.assertEqual(
+            data['Black Mirror'],
+            'https://www.tusubtitulo.com/show/1168'
+        )
+
+    def test_season_page_parser(self):
+        url = tusubtitulo.SEASON_PAGE_PATTERN.format(show='1093', season='5')
+        data = tusubtitulo.parse_season_page(mock_fetcher(url))
+
+        self.assertTrue((
+            '10',
+            'She Gets Revenge',
+            'WEB-DL',
+            'Español (España)',
+            'https://www.tusubtitulo.com/updated/5/47505/1') in data)
+
+
+class APITest(unittest.TestCase):
+    def setUp(self):
+        self.api = MockedAPI()
+
+    def test_exact_match(self):
+        info = self.api.get_show('Black Mirror')
         self.assertEqual(info.id, '1168')
+        self.assertEqual(
+            info.url,
+            tusubtitulo.SERIES_PAGE_PATTERN.format(show='1168'))
 
-    def test_series_finder_lower(self):
-        info = TestShowInfo('z nation')
+    def test_lowercase_match(self):
+        info = self.api.get_show('z nation')
         self.assertEqual(info.id, '2201')
+        self.assertEqual(info.title, 'Z Nation')
 
-    def test_series_finder_similar_1(self):
-        info = TestShowInfo('Hawaii Five 0')
+    def test_lowercase_similar_1(self):
+        info = self.api.get_show('hawaii five 0')
         self.assertEqual(info.id, '695')
 
-    def test_series_finder_similar_2(self):
-        info = TestShowInfo('mad man')
+    def test_lowercase_similar_2(self):
+        info = self.api.get_show('mad man')
         self.assertEqual(info.id, '79')
+        self.assertEqual(info.title, 'Mad Men')
 
-    def test_season_info(self):
-        info = TestShowInfo('American Horror Story', id='1093')
-        subs = info.get_season_subtitles(5)
-        print(repr(subs))
+    def test_missing_series(self):
+        with self.assertRaises(tusubtitulo.ShowNotFoundError):
+            self.api.get_show('foo')
+
+    def test_subtitles_info(self):
+        info = self.api.get_subtitles('American Horror Story', '5', '3')
+        self.assertEqual(len(info), 5)
+
+        es_ES = [x for x in info if x.language == 'es-ES']
+        self.assertEqual(len(es_ES), 3)
+
+# class TuSubtituloTest(unittest.TestCase):
+#     def test_series_finder_exact(self):
+#         info = TestShowInfo('Black Mirror')
+#         self.assertEqual(info.id, '1168')
+
+#     def test_series_finder_lower(self):
+#         info = TestShowInfo('z nation')
+#         self.assertEqual(info.id, '2201')
+
+#     def test_series_finder_similar_1(self):
+#         info = TestShowInfo('Hawaii Five 0')
+#         self.assertEqual(info.id, '695')
+
+#     def test_series_finder_similar_2(self):
+#         info = TestShowInfo('mad man')
+#         self.assertEqual(info.id, '79')
+
+#     def test_season_info(self):
+#         info = TestShowInfo('American Horror Story', id='1093')
+#         subs = info.get_season_subtitles(5)
+#         print(repr(subs))
 
 if __name__ == '__main__':
     unittest.main()
--- tusubtitulo.py
+++ tusubtitulo.py
@@ -1,95 +1,202 @@
 import difflib
 import re
+import io
+import gzip
 from urllib import request
 
 import bs4
 
 _NETWORK_ENABLED = True
 
+MAIN_URL = 'https://www.tusubtitulo.com/'
+SERIES_INDEX_URL = MAIN_URL + 'series.php'
+SERIES_PAGE_PATTERN = MAIN_URL + 'show/{show}'
+SEASON_PAGE_PATTERN = (MAIN_URL +
+                       'ajax_loadShow.php?show={show}&season={season}')
+
+
+class API:
+    def __init__(self, fetch_func=None):
+        if fetch_func is None:
+            fetch_func = fetch_url
+        self._fetch_func = fetch_func
+
+    def fetch(self, url):
+        return self._fetch_func(url)
+
+    def get_show(self, show):
+        def _get_id_from_url(url):
+            m = re.match(
+                MAIN_URL + 'show/(\d+)',
+                url,
+                flags=re.IGNORECASE)
+
+            if not m:
+                raise ValueError(url)
+
+            return m.group(1)
+
+        # Search exact match
+        table = parse_index_page(
+            self.fetch(SERIES_INDEX_URL),
+            asdict=True)
+        rev = {v: k for (k, v) in table.items()}
+
+        if show in table:
+            return ShowInfo(
+                title=show,
+                id=_get_id_from_url(table[show]),
+                url=table[show])
+
+        # Search by lowercase
+        lc_table = {show.lower(): link for (show, link) in table.items()}
+        lc_show = show.lower()
+        if lc_show in lc_table:
+            url = lc_table[lc_show]
+            return ShowInfo(
+                title=rev[url],
+                id=_get_id_from_url(url),
+                url=url)
+
+        # Aproximate search
+        ratios = [
+            (key, difflib.SequenceMatcher(None, lc_show, key).ratio())
+            for key in lc_table
+        ]
+
+        ratios = reversed(sorted(ratios, key=lambda x: x[1]))
+        first = next(ratios)
+        if first[1] >= 0.75:
+            url = lc_table[first[0]]
+            return ShowInfo(
+                title=rev[url],
+                id=_get_id_from_url(url),
+                url=url)
+
+        raise ShowNotFoundError(show)
+
+    def get_subtitles(self, show, season, episode=None):
+        language_table = {
+            'english': 'en-US',
+            'español (españa)': 'es-ES',
+            'español (latinoamérica)': 'es-LAT'
+        }
+
+        showinfo = self.get_show(show)
+
+        season_data = parse_season_page(self.fetch(
+            SEASON_PAGE_PATTERN.format(show=showinfo.id, season=season)))
+
+        ret = []
+        for (ep, title, version, language, url) in season_data:
+            if ep is not None and ep != episode:
+                continue
+
+            try:
+                language = language_table[language.lower()]
+            except KeyError:
+                continue
+
+            ret.append(SubtitleInfo(
+                show=showinfo,
+                season=season,
+                ep=ep,
+                version=version,
+                language=language,
+                url=url,
+                title=title))
+
+        return ret
+
 
 class ShowInfo:
-    def __init__(self, show=None, id=None, fetch=None):
-        self.show = show
+    def __init__(self, title, id, url):
+        self.title = title
         self.id = id
 
-        if fetch is None:
-            fetch = fetch_url
-        self._fetch = fetch
+        assert self.url == url
 
-        if self.show is None and self.id is None:
-            raise ValueError('show or id is needed')
-
-        if not self.id:
-            self._get_show_info()
-
-    def _get_show_info(self):
-        url = get_show_url(
-            self.show,
-            buff=self._fetch('https://www.tusubtitulo.com/series.php'))
-
-        m = re.match(
-            'https://www.tusubtitulo.com/show/(\d+)',
-            url,
-            flags=re.IGNORECASE)
-        if not m:
-            raise SeriesNotFoundError(self.show)
-
-        self.id = m.group(1)
-
-    def get_episode_subtitles(self, season, episode):
-        if season not in self._subtitle_info:
-            self.fetch_season_info(season)
-
-        return self._subtitle_info[season][episode]
-
-    def fetch_season_info(self, season):
-        season_url = ('https://www.tusubtitulo.com/ajax_loadShow.php?'
-                      'show={show_id}&season={season}')
-        season_url = season_url.format(show_id=self.id, season=season)
-
-        return season_url
+    @property
+    def url(self):
+        return SERIES_PAGE_PATTERN.format(show=self.id)
 
 
-    def get_season_subtitles(self, season):
-        season_url = self.get_season_url(season)
-        subtitles = parse_season_page(self._fetch(season_url))
+class SubtitleInfo:
+    def __init__(self, show, season, ep, version, language, url, title=None):
+        self.show = show
+        self.season = season
+        self.episode = ep
+        self.title = title
+        self.version = version
+        self.language = language
+        self.url = url
+
+    def __repr__(self):
+        fmt = (
+            "<"
+            "{mod}.{cls} "
+            "{show} s:{season} e:{episode} ({language}/{version}) "
+            "{url}>")
+
+        return fmt.format(
+            mod=__name__,
+            cls=self.__class__.__name__,
+            show=self.show.title,
+            language=self.language,
+            season=self.season,
+            episode=self.episode,
+            version=self.version,
+            url=self.url)
+
+#         if fetch is None:
+#             fetch = fetch_url
+#         self._fetch = fetch
+
+#         if self.show is None and self.id is None:
+#             raise ValueError('show or id is needed')
+
+#         if not self.id:
+#             self._get_show_info()
+
+    # def _get_show_info(self):
+    #     url = get_show_url(
+    #         self.show,
+    #         buff=self._fetch(self._SERIES_INDEX))
+
+    #     m = re.match(
+    #         'https://www.tusubtitulo.com/show/(\d+)',
+    #         url,
+    #         flags=re.IGNORECASE)
+    #     if not m:
+    #         raise ShowNotFoundError(self.show)
+
+    #     self.id = m.group(1)
+
+    # def get_episode_subtitles(self, season, episode):
+    #     if season not in self._subtitle_info:
+    #         self.fetch_season_info(season)
+
+    #     return self._subtitle_info[season][episode]
+
+    # def fetch_season_info(self, season):
+    #     season_url = ('https://www.tusubtitulo.com/ajax_loadShow.php?'
+    #                   'show={show_id}&season={season}')
+    #     season_url = season_url.format(show_id=self.id, season=season)
+
+    #     return season_url
+
+
+    # def get_season_subtitles(self, season):
+    #     season_url = self.get_season_url(season)
+    #     subtitles = parse_season_page(self._fetch(season_url))
 
 
 
-class SeriesNotFoundError(Exception):
+class ShowNotFoundError(Exception):
     def __init__(self, series, *args, **kwargs):
         self.show = series
         super().__init__(self, *args, **kwargs)
 
-#
-# High level functions
-#
-
-
-def get_show_url(series, buff):
-    if buff is None:
-        buff = fetch_url('https://www.tusubtitulo.com/series.php')
-
-    table = {series: link for (series, link) in parse_index_page(buff)}
-    if series in table:
-        return table[series]
-
-    lc_table = {series.lower(): link for (series, link) in table.items()}
-    lc_series = series.lower()
-    if lc_series in lc_table:
-        return lc_table[lc_series]
-
-    ratios = [
-        (key, difflib.SequenceMatcher(None, lc_series, key).ratio())
-        for key in lc_table
-    ]
-
-    ratios = reversed(sorted(ratios, key=lambda x: x[1]))
-    first = next(ratios)
-    if first[1] >= 0.75:
-        return lc_table[first[0]]
-
-    raise SeriesNotFoundError(series)
 
 
 #
@@ -100,16 +207,19 @@ def _soupify(buff, encoding='utf-8', parser="html.parser"):
     return bs4.BeautifulSoup(buff.decode(encoding), parser)
 
 
-def parse_index_page(buff):
+def parse_index_page(buff, asdict=False):
     soup = _soupify(buff)
-    series_links = [
+    
+    ret = [
         (x.text, 'https://www.tusubtitulo.com' + x.attrs['href'])
         for x in soup.select('a')
         if x.attrs.get('href', '').startswith('/show/')
     ]
 
-    return series_links
+    if asdict:
+        ret = {show: url for (show, url) in ret}
 
+    return ret
 
 def parse_season_page(buff):
     ret = []
@@ -138,13 +248,13 @@ def parse_season_page(buff):
             curr_ep = ep
             curr_title = title
 
-            print("Got episode {} header: {}".format(ep, title))
+            # print("Got episode {} header: {}".format(ep, title))
 
         # Version header
         elif td.attrs.get('colspan', '') == '3':
             curr_version = td.text.strip()
             curr_version = curr_version.split(' ', maxsplit=1)[1]
-            print("  Version:", curr_version)
+            # print("  Version:", curr_version)
 
         elif 'language' in td.attrs.get('class', []):
             language = td.text.strip()
@@ -171,10 +281,13 @@ def parse_season_page(buff):
                 href
             ))
 
+    return ret
 
 #
 # Network
 #
+
+
 def fetch_url(url, headers=None):
     if not _NETWORK_ENABLED:
         raise RuntimeError('Network not enabled')
